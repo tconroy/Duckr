dc911eebae3b41f7a5c4456db56111ddb34302f8
Tom Conroy <tom@thomasconroy.net>
2016-08-05 20:45:49 -0400

    hooked up redux devtools

--- app/index.js
+++ app/index.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import getRoutes from './config/routes'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 import users from 'redux/modules/users'
 import thunk from 'redux-thunk'
 import { checkIfAuthed } from 'helpers/auth'
 
 // create store, binding in thunk middleware
-const store = createStore(users, applyMiddleware(thunk))
+const store = createStore(users, compose(
+  applyMiddleware(thunk),
+  window.devToolsExtension ? window.devToolsExtension() : (f) => f
+))
 
 function checkAuth (nextState, replace) {
   if (store.getState().isFetching) {
